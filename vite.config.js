16a466a3111f0ddb0150855620e45b10c4bc196e
Akash Wagh <Akash.Wagh@esds.co.in>
2025-12-17 19:06:11 +0530

    Get master

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/cidco/',
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    strictPort: false,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
 })
